66e169edd01187b7701197c0738d4687835760e8
sugawara-kazuya <kz515yssg@gmail.com>
2024-08-19 15:08:40 +0900

    path修正

--- src/app/admin/[id]/page.tsx
+++ src/app/admin/[id]/page.tsx
@@ -151,7 +151,7 @@ export default function EventDetail() {
       //   "https://amplify-moshimoji-root-sandbox-1-teabucket26470cb4-m9df2bygeb2t.s3.ap-northeast-1.amazonaws.com/";
       // dev
       const baseUrl =
-        "https://amplify-d2zzbrnh9ajitz-dev-branc-teabucket26470cb4-ttvps8lvvdyb .s3.ap-northeast-1.amazonaws.com/";
+        "https://amplify-d2zzbrnh9ajitz-dev-branc-teabucket26470cb4-ttvps8lvvdyb.s3.ap-northeast-1.amazonaws.com/";
 
       if (selectedFile) {
         const path = `event/${selectedFile.name}`;
