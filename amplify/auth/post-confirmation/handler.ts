3ce958886470383cb814bf30eca11aae63bb28b5
sugawara-kazuya <kz515yssg@gmail.com>
2025-02-12 00:06:29 +0900

    fix:認証追加

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -2,14 +2,9 @@ import type { PostConfirmationTriggerHandler } from "aws-lambda";
 import { type Schema } from "../../data/resource";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
-import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
-import { env } from "$amplify/env/post-confirmation";
+import outputs from "../../../amplify_outputs.json";
 
-// const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(
-//   env
-// );
-
-// Amplify.configure(resourceConfig, libraryOptions);
+Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
