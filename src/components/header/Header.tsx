aaa1da3c2361b6b708622ae4876b01368eacb8d6
sugawara-kazuya <kz515yssg@gmail.com>
2025-02-12 01:21:34 +0900

    fix:マイページ

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -83,7 +83,7 @@ export const Header: React.FC<HeaderProps> = ({ backgroundImage }) => {
               className="text-white font-semibold py-2 md:py-0 hover:text-yellow-400 transition-colors duration-200"
               onClick={() => handleNavigation("/mypage")}
             >
-              マイページ
+              あなたのページ
             </a>
           </div>
         </div>
