0433b1b8b8ab0b11132f566ad54234a914a80682
Masakazu-Adachi <m-adachi@sustirel.com>
2025-04-26 22:54:39 +0900

    Update client-layout.tsx

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,16 +1,21 @@
-'use client';
+'use client'
 
-import { Amplify } from 'aws-amplify';
-import outputs from '@/output';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { Authenticator, translations } from '@aws-amplify/ui-react'
+import { I18n } from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css'
+import React from 'react'
 
-Amplify.configure(outputs);
+I18n.putVocabularies(translations)
+I18n.setLanguage('ja')
 
-export function ClientLayout({ children }: { children: React.ReactNode }) {
+type Props = {
+  children: React.ReactNode
+}
+
+export default function ClientLayout({ children }: Props) {
   return (
     <Authenticator.Provider>
       {children}
     </Authenticator.Provider>
-  );
-} 
+  )
+}
